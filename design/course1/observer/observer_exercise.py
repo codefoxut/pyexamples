f1b99c938031b97f073301d12fce027573346b82
Ujjwal Tak <ujjwal.tak@go-mmt.com>
2020-11-21 20:46:08 +0530

    observer pattern

---
+++ design/course1/observer/observer_exercise.py
@@ -0,0 +1,12 @@
+class Game:
+    def __init__(self):
+        # todo
+        pass
+
+
+class Rat:
+    def __init__(self, game):
+        self.game = game
+        self.attack = 1
+        # todo
+        
