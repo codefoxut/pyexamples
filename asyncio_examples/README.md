3fcfecf7db2e314b994c1c13125ec1e33e31d7f0
Ujjwal Tak <ujjwal.tak@go-mmt.com>
2020-12-19 23:28:48 +0530

    asyncio commit

---
+++ asyncio_examples/README.md
@@ -0,0 +1,62 @@
+Asyncio
+=======
+
+
+Functions:
+---------
+1. **asyncio.run**(coro, *, debug=False): _Execute the coroutine coro and return the result._
+   - This function cannot be called when another asyncio event loop is running in the same thread.
+   - This function always creates a new event loop and closes it at the end. It should be used as a main entry point 
+   for asyncio programs, and should ideally only be called once
+2. **asyncio.create_task**(coro, *, name=None): _Wrap the coro coroutine into a Task and schedule its execution. 
+   Return the Task object._
+3. _coroutine_ **asyncio.sleep**(delay, result=None, *, loop=None): _Block for delay seconds_.
+4. _awaitable_ **asyncio.gather**(*aws, loop=None, return_exceptions=False): _Run awaitable objects in the aws 
+   sequence concurrently._
+5. _coroutine_ **asyncio.wait_for**(aw, timeout, *, loop=None): _Wait for the aw awaitable to complete with a timeout._
+6. _awaitable_ **asyncio.shield**(aw, *, loop=None): _Protect an awaitable object from being cancelled._
+7. _coroutine_ **asyncio.wait**(aws, *, loop=None, timeout=None, return_when=ALL_COMPLETED):
+    _Run awaitable objects in the aws iterable concurrently and block until the condition specified by return_when._
+   - Returns two sets of Tasks/Futures: (done, pending).
+   > done, pending = await asyncio.wait(aws)
+   - this function does not raise `asyncio.TimeoutError`. Futures or Tasks that aren’t done when the timeout occurs are
+     simply returned inside the second set.
+   
+        | Constant | Description  |
+        | -------- | ------------ |
+        | FIRST_COMPLETED | The function will return when any future finishes or is cancelled. |
+        | FIRST_EXCEPTION | The function will return when any future finishes by raising an exception. If no future raises an exception then it is equivalent to ALL_COMPLETED. |
+        | ALL_COMPLETED | The function will return when all futures finish or are cancelled. |
+   - The explicit passing of coroutine objects to asyncio.wait() is deprecated.
+8. **asyncio.as_completed**(aws, *, loop=None, timeout=None): _Run awaitable objects in the aws iterable concurrently. 
+   Return an iterator of coroutines. Each coroutine returned can be awaited to get the earliest next result 
+   from the iterable of the remaining awaitables._
+9. _coroutine_ **asyncio.to_thread**(func, /, *args, **kwargs): _Asynchronously run function func in a separate thread._
+    * New in version 3.9
+    * This coroutine function is primarily intended to be used for executing IO-bound functions/methods that 
+      would otherwise block the event loop if they were run in the main thread.
+10. **asyncio.run_coroutine_threadsafe**(coro, loop): _Submit a coroutine to the given event loop. Thread-safe._
+11. **asyncio.all_tasks**(loop=None): _Return a set of not yet finished Task objects run by the loop._
+12. **asyncio.current_task**(loop=None): _Return the currently running Task instance, or None if no task is running._
+13. _class_ **asyncio.Task**(coro, *, loop=None, name=None) _A Future-like object that runs a Python coroutine._
+    - Not thread-safe.
+    - asyncio.Task inherits from Future all of its APIs except Future.set_result() and Future.set_exception().
+    - **cancel**(msg=None): Request the Task to be cancelled.
+14. **asyncio.iscoroutine**(obj): _Return True if obj is a coroutine object._  
+15. **asyncio.iscoroutinefunction**(func): _Return True if func is a coroutine function._
+
+Awaitables:
+----------
+- An object is an awaitable object if it can be used in an await expression.
+- There are three main types of awaitable objects: `coroutines`, `Tasks`, and `Futures`.
+    - **Coroutines**:
+        + Python coroutines are awaitables.
+    - **Tasks**:
+        + Tasks are used to schedule coroutines concurrently.
+    - **Futures**:
+        + A Future is a special low-level awaitable object that represents an eventual result of 
+          an asynchronous operation.
+        + Future objects in asyncio are needed to allow callback-based code to be used with async/await.
+
+
+
---
+++ asyncio_examples/asyncio_tasks.py
@@ -0,0 +1,197 @@
+import asyncio
+import time
+from datetime import datetime
+
+
+async def hello_world():
+    print("hello")
+    await asyncio.sleep(1)
+    print("world")
+
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+
+async def hello_world2():
+    print(f"started at {time.strftime('%X')}")
+
+    await say_after(1, 'hello')
+    print(f"after hello at {time.strftime('%X')}")
+    await say_after(2, 'world')
+
+    print(f"finished at {time.strftime('%X')}")
+
+
+async def hello_world3():
+    task1 = asyncio.create_task(
+        say_after(1, 'hello'))
+
+    task2 = asyncio.create_task(
+        say_after(2, 'world'))
+
+    print(f"started at {time.strftime('%X')}")
+
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    await task1
+    await task2
+
+    print(f"finished at {time.strftime('%X')}")
+
+
+async def display_date():
+    loop = asyncio.get_running_loop()
+    end_time = loop.time() + 5.0
+    while True:
+        print(datetime.now())
+        if (loop.time() + 1.0) >= end_time:
+            break
+        await asyncio.sleep(1)
+
+
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number + 1):
+        print(f"Task {name}: Compute factorial({i})...")
+        await asyncio.sleep(1)
+        f *= i
+    print(f"Task {name}: factorial({number}) = {f} at {time.strftime('%X')}")
+
+
+async def try_gather():
+    # Schedule three calls *concurrently*:
+    await asyncio.gather(
+        factorial("A", 2),
+        factorial("B", 3),
+        factorial("C", 4),
+    )
+
+
+async def eternity():
+    # Sleep for one hour
+    await asyncio.sleep(3600)
+    print('yay!')
+
+
+async def check_timeout():
+    # Wait for at most 1 second
+    try:
+        await asyncio.wait_for(eternity(), timeout=1.0)
+    except asyncio.TimeoutError:
+        print('timeout!')
+
+
+async def foo():
+    return 42
+
+
+async def test_failed_wait():
+    coro = foo()
+    done, pending = await asyncio.wait({coro})
+
+    if coro in done:
+        print(coro)
+        # This branch will never be run!
+
+
+async def test_wait_passed():
+    task = asyncio.create_task(foo())
+    done, pending = await asyncio.wait({task})
+
+    if task in done:
+        print(f"{task.done()} result:{task.result()} is done")
+        # Everything will work as expected now.
+
+
+def blocking_io():
+    print(f"start blocking_io at {time.strftime('%X')}")
+    # Note that time.sleep() can be replaced with any blocking
+    # IO-bound operation, such as file operations.
+    time.sleep(2)
+    print(f"blocking_io complete at {time.strftime('%X')}")
+
+
+async def check_thread():
+    print(f"started main at {time.strftime('%X')}")
+
+    await asyncio.gather(
+        asyncio.to_thread(blocking_io),
+        asyncio.sleep(1),
+        hello_world3()
+    )
+
+    print(f"finished main at {time.strftime('%X')}")
+
+
+async def cancel_me():
+    print('cancel_me(): before sleep')
+
+    try:
+        # Wait for 1 hour
+        await asyncio.sleep(3600)
+    except asyncio.CancelledError:
+        print('cancel_me(): cancel sleep')
+        raise
+    finally:
+        print('cancel_me(): after sleep')
+
+
+async def check_cancel_me():
+    # Create a "cancel_me" Task
+    task = asyncio.create_task(cancel_me())
+
+    # Wait for 1 second
+    await asyncio.sleep(1)
+
+    task.cancel()
+    try:
+        await task
+    except asyncio.CancelledError:
+        print("main(): cancel_me is cancelled now")
+
+
+async def check_threadsafe():
+    loop = asyncio.get_event_loop()
+    timeout = 3
+
+    # Create a coroutine
+    coro = asyncio.sleep(1, result=3)
+
+    # Submit the coroutine to a given loop
+    future = asyncio.run_coroutine_threadsafe(coro, loop)
+
+    # Wait for the result with an optional timeout argument
+    try:
+        result = future.result(timeout)
+    except asyncio.TimeoutError:
+        print('The coroutine took too long, cancelling the task...')
+        future.cancel()
+    except Exception as exc:
+        import traceback
+        traceback.print_exc()
+        print(f'The coroutine raised an exception: {exc!r}')
+    else:
+        print(f'The coroutine returned: {result!r}')
+
+
+def run_coroutine1():
+    # asyncio.run(hello_world())
+    # asyncio.run(hello_world2())
+    # asyncio.run(hello_world3())
+    # asyncio.run(display_date())
+    # asyncio.run(try_gather())
+    # asyncio.run(check_timeout())
+    asyncio.run(test_failed_wait())
+    asyncio.run(test_wait_passed())
+    asyncio.run(check_cancel_me())
+    asyncio.run(check_thread())
+    asyncio.run(check_threadsafe())
+
+
+if __name__ == '__main__':
+    # hello world
+    run_coroutine1()
+    # print(hello_world())
+    # Throws RuntimeWarning: coroutine 'hello_world' was never awaited
